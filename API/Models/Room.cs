2a60eb488c2d6d3a23638dd922144463f18fba19
FahrulDems <damesfahrul2@gmail.com>
2023-06-21 13:07:48 +0700

    Models

---
+++ API/Models/Room.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+[Table("tb_tr_rooms")]
+public class Room
+{
+    [Key]
+    [Column("guid")]
+    public Guid Guid { get; set; }
+
+    [Column("name", TypeName = "nvarchar(100)")]
+    public string Name { get; set; }
+
+    [Column("floor")]
+    public int Floor { get; set; }
+
+    [Column("capacity")]
+    public int Capacity { get; set; }
+
+    [Column("created_date")]
+    public DateTime CreatedDate { get; set; }
+
+    [Column("modified_date")]
+    public DateTime ModifiedDate { get; set; }
+}
