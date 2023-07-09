b080adec546433775875fac7cdc38b3a4d85c54a
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-07-09 22:52:33 +0700

    Consume API Pokemon/ditto

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -59,19 +59,158 @@ let getFakultasKomputer = (arrayName) => {
 }
 getFakultasKomputer(arrayMhsObj);
 
+$.ajax({
+    url: "https://pokeapi.co/api/v2/pokemon/"
+}).done((result) => {
+    console.log(result.results);
+    let temp = "";
+    $.each(result.results, (key, val) => {
+        temp += `<tr>
+                    <td>${key + 1}</td>
+                    <td>${val.name}</td>
+                    <td><button onclick="detail('${val.url}')" data-bs-toggle="modal" data-bs-target="#modal" class="btn btn-info">Detail</button></td>
+                </tr>`;
+    })
+    $("#tbody").html(temp);
+    console.log(temp);
+})
 
 
+function detail(stringURL) {
+    $.ajax({
+        url: stringURL
+    }).done(res => {
+        $(".modal-title").html(res.name);
 
-/*let validateIsActive = (arrayName) => {
-    for (let i = 0; i < arrayName.length; i++)
-    {
-        if (parseInt(arrayName[i].nim.slice(-2)) >= 30)
-        {
-            arrayName[i].isActive = false;
-        }
-    }
-    console.log(arrayName);
+        $("#image-front").attr("src", res.sprites.other.dream_world.front_default);
+
+        let stats = "";
+        res.stats.forEach(stat => {
+            let percentage = (stat.base_stat / 255) * 100; // Menghitung persentase
+            let statBar = `<div class="stat-bar">
+                                <div class="stat-fill" style="width: ${percentage}%"></div>
+                           </div>`;
+            stats += `<div style="margin-bottom: 1px; background-color: white; border-radius: 8px">${stat.stat.name + ": " + stat.base_stat} ${statBar}</div>`;
+        });
+        $(".stats").html(stats);
+
+        let abilities = "";
+        res.abilities.forEach(ability => {
+            let color = "";
+            let backgroundColor = "";
+            if (ability.ability.name === "overgrow") {
+                backgroundColor = "#006400"; // Hijau gelap
+                color = "white";
+            } else if (ability.ability.name === "chlorophyll") {
+                backgroundColor = "#32CD32"; // Hijau terang
+                color = "white";
+            } else if (ability.ability.name === "blaze") {
+                backgroundColor = "#FF4500"; // Merah terang
+                color = "white";
+            } else if (ability.ability.name === "solar") {
+                backgroundColor = "#FFD700"; // Kuning
+                color = "white";
+            } else if (ability.ability.name === "torrent") {
+                backgroundColor = "#1E90FF"; // Biru laut
+                color = "white";
+            } else if (ability.ability.name === "rain-dish") {
+                backgroundColor = "#87CEEB"; // Biru langit
+                color = "black";
+            } else if (ability.ability.name === "shield-dust") {
+                backgroundColor = "#C0C0C0"; // Abu-abu perak
+                color = "black";
+            } else if (ability.ability.name === "run-away") {
+                backgroundColor = "#8B4513"; // Cokelat
+                color = "white";
+            } else if (ability.ability.name === "shed-skin") {
+                backgroundColor = "#F5F5DC"; // Kulit
+                color = "black";
+            } else if (ability.ability.name === "compound-eyes") {
+                backgroundColor = "#4B0082"; // Ungu gelap
+                color = "white";
+            } else if (ability.ability.name === "swarm") {
+                backgroundColor = "#B8860B"; // Kuning gelap
+                color = "white";
+            } else if (ability.ability.name === "sniper") {
+                backgroundColor = "#000080"; // Biru tua
+                color = "white";
+            } else if (ability.ability.name === "keen-eye") {
+                backgroundColor = "#FFFF00"; // Kuning muda
+                color = "black";
+            } else if (ability.ability.name === "tangled-feet") {
+                backgroundColor = "#D3D3D3"; // Abu-abu muda
+                color = "black";
+            } else if (ability.ability.name === "big-pecks") {
+                backgroundColor = "#FFC0CB"; // Merah muda
+                color = "black";
+            } else if (ability.ability.name === "guts") {
+                backgroundColor = "#FF0000"; // Merah
+                color = "white";
+            } else if (ability.ability.name === "hustle") {
+                backgroundColor = "#696969"; // Abu-abu tua
+                color = "white";
+            } else {
+                backgroundColor = "#0000FF"; // Warna default jika tidak ada yang cocok
+                color = "white";
+            }
+            abilities += `<span style="margin: 5px; display: inline-block; text-align: center; background-color:${backgroundColor}; color: ${color}; border-radius: 8px; padding: 3px; font-family: 'Comic Sans MS'">${ability.ability.name}</span>`;
+        });
+        $("#abilities").html(abilities);
+
+        let types = "";
+        res.types.forEach(type => {
+            let backgroundColor = ""; // Warna latar belakang
+            let color = "";
+            let iconClass = ""; // Kelas ikon Font Awesome
+            if (type.type.name === "fire") {
+                backgroundColor = "#FF4500"; // Merah terang
+                color = "white";
+                iconClass = "fas fa-fire"; // Ikon api
+            } else if (type.type.name === "water") {
+                backgroundColor = "#1E90FF"; // Biru laut
+                color = "white";
+                iconClass = "fas fa-water"; // Ikon air
+            } else if (type.type.name === "grass") {
+                backgroundColor = "#228B22"; // Hijau daun
+                color = "white";
+                iconClass = "fas fa-leaf"; // Ikon rumput
+            } else if (type.type.name === "poison") {
+                backgroundColor = "#9932CC"; // Ungu tua
+                color = "white";
+                iconClass = "fas fa-skull-crossbones"; // Ikon racun
+            } else if (type.type.name === "flying") {
+                backgroundColor = "#87CEEB"; // Biru langit
+                color = "black";
+                iconClass = "fas fa-dove"; // Ikon terbang
+            } else if (type.type.name === "bug") {
+                backgroundColor = "#9ACD32"; // Hijau cerah
+                color = "black";
+                iconClass = "fas fa-bug"; // Ikon serangga
+            } else if (type.type.name === "normal") {
+                backgroundColor = "#A9A9A9"; // Abu-abu tua
+                color = "black";
+                iconClass = "fas fa-circle"; // Ikon normal
+            } else {
+                backgroundColor = "#000000"; // Warna default jika tidak ada yang cocok
+                color = "white";
+                iconClass = "fas fa-question"; // Ikon default jika tidak ada yang cocok
+            }
+
+            types += `<span style="margin: 5px; display: inline-block; text-align: center; background-color: ${backgroundColor}; color: ${color}; border-radius: 8px; padding: 3px; font-family: 'Comic Sans MS'"><i class="${iconClass}"></i> ${type.type.name}</span>`;
+        });
+        $("#types").html(types);
+
+        let weight = res.weight
+        let weightFormatted = (weight/10).toLocaleString('en-US');
+        weightFormatted = weightFormatted.replace('.', ',');
+
+        $("#weight").html(weightFormatted);
+
+        let height = res.height
+        let heightFormatted = (height / 10).toLocaleString('en-US');
+        heightFormatted = heightFormatted.replace('.', ',');
+        $("#height").html(heightFormatted);
+
+        $("#base-experience").html(res.base_experience);
+    });
 }
-
-validateIsActive(arrayMhsObj);
-*/
