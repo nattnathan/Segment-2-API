75cd8bb243d5983bb16525da5f85515a5d93ff24
FahrulDems <136286061+FahrulDems@users.noreply.github.com>
2023-07-12 20:36:17 +0700

    chart

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -515,6 +515,112 @@ function Update() {
     });
 }
 
+function showGenderChart() {
+    // Mendapatkan data dari API
+    $.ajax({
+        url: "https://localhost:7256/api/employees", // Sesuaikan URL sesuai dengan endpoint API Anda
+        type: "GET",
+        dataType: "json"
+    }).done(res => {
+        // Mendapatkan jumlah jenis kelamin
+        let femaleCount = 0;
+        let maleCount = 0;
+        for (let i = 0; i < res.data.length; i++) {
+            if (res.data[i].gender === 0) {
+                femaleCount++;
+            } else if (res.data[i].gender === 1) {
+                maleCount++;
+            }
+        }
+
+        // Menghitung total data
+        let totalCount = femaleCount + maleCount;
+
+        // Menghitung persentase jenis kelamin
+        let femalePercentage = (femaleCount / totalCount) * 100;
+        let malePercentage = (maleCount / totalCount) * 100;
+
+        // Membuat grafik menggunakan Chart.js
+        let ctx = document.getElementById('genderChart').getContext('2d');
+        let genderChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Female', 'Male'],
+                datasets: [{
+                    data: [femalePercentage, malePercentage],
+                    backgroundColor: ['#FF6384', '#36A2EB'],
+                    hoverBackgroundColor: ['#FF6384', '#36A2EB']
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    callbacks: {
+                        label: function (tooltipItem, data) {
+                            let label = data.labels[tooltipItem.index];
+                            let value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+                            return label + ': ' + value.toFixed(2) + '% (' + Math.round(value * totalCount / 100) + ')';
+                        }
+                    }
+                }
+            }
+        });
+        // Membuka modal setelah grafik selesai dibuat
+        $('#chartModal').modal('show');
+    }).fail(error => {
+        alert("Failed to fetch data from API.");
+    });
+}
+
+function showUniversityChart() {
+    // Mendapatkan data dari API
+    $.ajax({
+        url: "https://localhost:7256/api/universities", // Sesuaikan URL sesuai dengan endpoint API Anda
+        type: "GET",
+        dataType: "json"
+    }).done(res => {
+        // Mendapatkan data universitas
+        let universities = res.data;
+
+        // Mengambil nama universitas dan kode universitas
+        let universityNames = universities.map(university => university.name);
+        let universityCodes = universities.map(university => university.code);
+
+        // Membuat grafik menggunakan Chart.js
+        let ctx = document.getElementById('universityChart').getContext('2d');
+        let universityChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: universityNames,
+                datasets: [{
+                    label: 'University Codes',
+                    data: universityCodes,
+                    backgroundColor: '#36A2EB',
+                    hoverBackgroundColor: '#36A2EB'
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        // Membuka modal setelah grafik selesai dibuat
+        $('#chartModal1').modal('show');
+    }).fail(error => {
+        alert("Failed to fetch data from API.");
+    });
+}
+
+
+
+
+
+
 
 
 
